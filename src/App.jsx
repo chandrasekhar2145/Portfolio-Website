dc699fc88f43df830c5547a69cf8306e11eb3f19
Chandrasekhar Kuruva <148301206+chandrasekhar2145@users.noreply.github.com>
2024-10-15 01:37:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Signup from './components/Signup';
-import Login from './components/Login';
 import Portfolio from './components/Portfolio';
  
 
@@ -14,10 +12,10 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/signup" element={<Signup />}>*/} 
+        {/*<Route path="/login" element={<Login />} />*/}
         <Route path="/portfolio" element= {<Portfolio/>} />
-        <Route path="*" element={<Navigate to="/login" />} />
+        <Route path="*" element={<Navigate to="/portfolio" />}/>
       </Routes>
     </Router>
   );
